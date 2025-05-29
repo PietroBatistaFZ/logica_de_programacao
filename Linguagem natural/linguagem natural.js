1f58f23a64bd8c35df7952488d0354a136a07ccb
Pietro <batistapietrofrancisco@gmail.com>
2025-05-29 13:46:53 -0300

    logica_de_programacao

---
+++ Linguagem natural/linguagem natural.js
@@ -0,0 +1,35 @@
+LOGPRO - Linguagem natural
+
+
+Trajeto até a escola: 
+Saindo de casa: 
+1. Eu desço a Rua dos Cravos. 
+2. Viro à direita na Rua Manoel de Abreu até o ponto de ônibus. Trajeto do ônibus (da Rua Manoel de Abreu até o destino final): 1. O ônibus sai da Rua Manoel de Abreu e vira à direita na Rua Raul Guerra. 2. Sigo reto e entro à esquerda na Rua Dirceu Ferrari. 
+3. Continuo adiante até a Rua Gabriel Fontana. 
+4. Viro suavemente à direita na Rua Carlos de Carvalho. 
+5. Dobre à esquerda na Rua Celso Santana. 
+6. Sigo em frente e entro à direita na Rua Hildebrando da Silveira. 7. Depois, viro à esquerda na Rua Lucy. 
+8. Continuo até a Rua Tiradentes, onde faço uma curva. 
+9. Finalmente, entro na Rua Armando da Fonseca, prosseguindo meu trajeto  até o Sesi/Escola. 
+Trajeto do ônibus (da Estação Ferraz de Vasconcelos até o Sesi/Escola): 
+1. O ônibus sai da Estação Ferraz de Vasconcelos e segue reto pela Avenida  Brasil por um bom trecho. 
+2. Vira à direita na Rua Presidente de Moraes e sobe essa rua. 3. Após subir, o ônibus vira à direita na Rua Princesa Isabel e sigo reto. 4. Em seguida, o ônibus vira à direita na Rua Nilo Peçanha. 5. Depois, o ônibus vira à direita na Rua Benedito Leite. 
+6. Seguindo pela Rua Benedito Leite, o ônibus vira à esquerda na Rua  Prudente de Morais. 
+7. O ônibus segue reto até a Rua Francisco Antônio Zeller, onde chega ao  Sesi/Escola.
+Passo a passo para fazer café  
+1. Coloco a quantidade de água na panela e deixo ferver. 
+2. Enquanto a água esquenta, coloco o filtro na cafeteira e ajeito bem. 
+3. Coloco o pó de café no filtro, geralmente de 4 a 6 colheres de sopa,  dependendo de quão forte eu queira no dia. 
+4. Quando a água estiver quase fervendo, despejo ela devagar sobre o pó de  café, fazendo movimentos circulares pra cobrir bem o pó. 
+5. Deixo a água passar pelo filtro e cair na cafeteira.. 
+6. Depois que o café terminar de coar, coloco 3 colheres de açúcar bem cheias. 7. E por fim, transfiro o café para a garrafa e me sirvo. 
+Receita de bolo de chocolate:: 
+1. Preaqueço o forno a 180°C e untar a forma com manteiga e farinha de trigo. 
+2. Em uma tigela, misturo 2 xícaras de farinha de trigo, 1 xícara de  achocolatado em pó, 1 xícara de açúcar e 1 colher de sopa de fermento em pó. 
+3. Em outra tigela, bato 3 ovos com 1 xícara de óleo até ficar bem misturado. 
+4. Coloco 1 xícara de leite e mexo mais um pouco até a mistura ficar bem  homogênea. 
+5. Junto os ingredientes secos com os líquidos e mexo com uma colher até  tudo se incorporar direitinho. 
+6. Despejo a massa na forma untada e levo ao forno por uns 30 a 35 minutos,  ou até o bolo passar no teste do palito (quando espetamos o palito e ele sai  limpo). 
+7. Enquanto o bolo assa, preparo a cobertura: coloco 1 lata de leite  condensado, 3 colheres de achocolatado em pó e 1 colher de manteiga em  uma panela. 
+8. Levo ao fogo baixo, mexo até começar a ferver e deixo por mais uns 2  minutinhos até ficar mais grossinha. 
+9. Retiro o bolo do forno, deixo esfriar um pouco e despejo a cobertura por  cima.
---
+++ fluxograma/fluxograma.js.js
@@ -0,0 +1,4 @@
+LOGPRO - Fluxograma
+
+
+https://miro.com/app/board/uXjVIfCyqbI=/
